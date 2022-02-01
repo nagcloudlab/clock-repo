09162a507e27c9bb0928fd6d26921b21f966cadf
nagabhushanam <nagabhushanamn@gmail.com>
2022-02-01 14:59:47 +0530

    no warnings

--- ui/src/Clock.js
+++ ui/src/Clock.js
@@ -1,12 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function Clock({ timeZone }) {
-    // const [time, setTime] = useState(new Date().toLocaleTimeString('en-US', { timeZone }))
-    // useEffect(() => {
-    //     setInterval(() => {
-    //         setTime(new Date().toLocaleTimeString('en-US', { timeZone }))
-    //     }, 1000);
-    // }, [])
     return (
         <div style={{ color: 'green' }}>
             {new Date().toLocaleTimeString('en-US', { timeZone })}
